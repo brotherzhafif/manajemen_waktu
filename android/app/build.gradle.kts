9f3ba33015319c97b9d2f834026985ea439694c0
brotherzhafif <bangz1504@gmail.com>
2025-07-12 16:57:30 +0700

    fix: task not showing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -28,7 +28,9 @@ android {
         applicationId = "com.example.manajemen_waktu"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        
+        // Set minimum SDK to 23 for Firebase compatibility
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
